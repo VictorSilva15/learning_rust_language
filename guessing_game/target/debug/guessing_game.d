3ee9e668e88076f54339e593bd0e5a6b2a4ff62b
Victor Hugo da Silva <victor.silvaPC@hotmail.com>
2021-12-04 17:28:43 -0300

    :tada: initial version

---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/victor/Área\ de\ Trabalho/rust/guessing_game/target/debug/guessing_game: /home/victor/Área\ de\ Trabalho/rust/guessing_game/src/main.rs
---
+++ guessing_game/target/rls/debug/deps/guessing_game-dae1996bbd1e616f.d
@@ -0,0 +1,5 @@
+/home/victor/Área de Trabalho/rust/guessing_game/target/rls/debug/deps/guessing_game-dae1996bbd1e616f.rmeta: src/main.rs
+
+/home/victor/Área de Trabalho/rust/guessing_game/target/rls/debug/deps/guessing_game-dae1996bbd1e616f.d: src/main.rs
+
+src/main.rs:

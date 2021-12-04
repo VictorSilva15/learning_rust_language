9a59d20bd35299ef824e42840c5efe7bd80ba544
Victor Hugo Da Silva <70340221+VictorSilva15@users.noreply.github.com>
2021-12-04 17:40:04 -0300

    Delete guessing_game/target directory

--- guessing_game/target/rls/debug/deps/guessing_game-02cb660b7ceba69d.d
+++
@@ -1,5 +0,0 @@
-/home/victor/Área de Trabalho/rust/guessing_game/target/rls/debug/deps/guessing_game-02cb660b7ceba69d.rmeta: src/main.rs
-
-/home/victor/Área de Trabalho/rust/guessing_game/target/rls/debug/deps/guessing_game-02cb660b7ceba69d.d: src/main.rs
-
-src/main.rs:

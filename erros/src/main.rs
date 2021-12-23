c08297bfe06be5f3a35c6e1c0fa00ec1b08da283
Victor Silva <victor.silvaPC@hotmail.com>
2021-12-23 10:58:06 -0300

    :zap: Errors Handling

---
+++ erros/src/main.rs
@@ -0,0 +1,32 @@
+//ERROR HANDLING 
+//
+//errors can be two types, recoverable and unrecoverable erros.
+//For recoverable errors, such as a file not found error. It's 
+//reasonable to report the problem to the user and retry the operation.
+//Unrecoverable erros are always symptoms of bugs, like trying to access
+//a location beyond the end of an array.
+
+
+//In some case we want stop/abort the program immediatelly, to do that, we need
+//to use the panic!() macro with the abort property. It's simple, just go to the
+//Cargo.toml and add `[profile.release]` and then `panic = 'abort'`
+
+
+fn main() {
+    let x = 12;
+
+    if x < 10 {
+        panic!("Error occured");
+    }
+    //The code above will print this:
+
+    //thread 'main' panicked at 'Error occured', src\main.rs:19:9
+    //note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace     
+    //error: process didn't exit successfully: `target\debug\erros.exe` (exit code: 101)
+
+    println!("x: {}", x);
+
+    //To handle the error and get more details of what are happens, you can use:
+    //`RUST_BACKTRACE=1` before the `cargo run` syntax to get a list of all the functions
+    //called to program runs
+}
